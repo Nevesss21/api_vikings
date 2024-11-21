f3bc26217467b0d46bcee0718c38e694b0737768
abner jose <abnerjose512@gmail.com>
2024-11-21 16:31:00 -0300

    .

--- src/repository/emailRepository.js
+++ src/repository/emailRepository.js
@@ -16,21 +16,21 @@ export async function enviarEmail(email, id) {
     to: email, // Destinatário (vem do parâmetro passado)
     subject: "Confirmação de Presença",
     html: `
-      <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px; padding: 20px; background-color: #f9f9f9;">
-        <h2 style="color: #4CAF50; text-align: center;">Confirmação de Presença</h2>
+      <div style="font-family: Arial, sans-serif; line-height: 1.6; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 8px; padding: 20px; background-color: #000; color: #cccccc;">
+        <h2 style="color: #A68D7A; text-align: center;">Confirmação de Presença</h2>
         <p>Olá,</p>
         <p>Seu número de identificação é: <strong>${id}</strong>.</p>
         <p>Clique no botão abaixo para confirmar sua presença na sessão:</p>
         <div style="text-align: center; margin: 20px 0;">
           <a href="http://4.172.207.208:3042/confirmar" 
-             style="display: inline-block; background-color: #4CAF50; color: white; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
+             style="display: inline-block; background-color: #A68D7A; color: white; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
             Confirmar Presença
           </a>
         </div>
-        <p style="font-size: 0.9em; color: #555;">Se você não solicitou este e-mail, ignore esta mensagem.</p>
+        <p style="font-size: 0.9em; color: #cccccc;">Se você não solicitou este e-mail, ignore esta mensagem.</p>
         <hr style="border: 0; border-top: 1px solid #ddd; margin: 20px 0;">
         <footer style="text-align: center; font-size: 0.8em; color: #888;">
-          &copy; 2024 Sua Empresa - Todos os direitos reservados.
+          &copy; 2024 Vikings - Todos os direitos reservados.
         </footer>
       </div>
     `,
